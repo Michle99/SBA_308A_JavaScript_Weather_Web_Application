635a0f8f820d287035ce2b10f416a6ff2216b596
Michle99 <sammiyonyx@gmail.com>
2023-11-05 13:58:57 -0500

    Added DOM elements to script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
 const apiKey = '4ae4a7e14072cdaa089d3848c195e1a7';
+
+// DOM elements
+const cityInput = document.getElementById('city-input');
+const stateInput = document.getElementById('state-input');
+const countryInput = document.getElementById('country-input');
+const searchButton = document.getElementById('search-button');
+const weatherCard = document.getElementById('weather-card');
+const cityName = document.getElementById('city-name');
+const weatherDescription = document.getElementById('weather-description');
+const weatherIcon = document.getElementById('weather-icon');
+const temperature = document.getElementById('temperature');
+const feelsLike = document.getElementById('feels-like');
+const pressure = document.getElementById('pressure');
+const tempMax = document.getElementById('temp-max');
+const tempMin = document.getElementById('temp-min');
+const sunrise = document.getElementById('sunrise');
+const sunset = document.getElementById('sunset');
+const forecast = document.getElementById('forecast');
